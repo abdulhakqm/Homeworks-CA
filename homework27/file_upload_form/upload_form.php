8ba42cb3aa1e65ddf2dff6f775b7ac6ba32b0817
abdulhakqm <61661820+abdulhakqm@users.noreply.github.com>
2020-05-24 23:42:55 +0200

    hw

---
+++ homework27/file_upload_form/upload_form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>File upload</title>
+</head>
+<body>
+    <h1>File upload form</h1>
+    
+    <form action="upload_file.php" method="POST" enctype="multipart/form-data" class="form-group">
+ 
+        <label for="fileUp">Select files to upload:</label>
+        <input type="file" name="fileWeUpload" id="fileUp" class="form-control w-25 my-2 bg-dark text-light"> <br>
+        <input type="submit" value="Upload file" name="submit" class="form-control w-25 outline-light bg-success text-light">
+    </form>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
